7219d7d68b5b0401261dcbeebf54139abe7a59ec
DzikuVx <pspychalski@gmail.com>
2015-01-02 20:37:53 +0100

    python code

---
+++ power_cutter.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+def main():
+	# GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(18, GPIO.OUT)
+
+	GPIO.output(18, GPIO.HIGH)
+	time.sleep(5);
+	GPIO.output(18, GPIO.LOW)
+
+	GPIO.cleanup()
+
+if __name__ == "__main__":
+	main()
+
